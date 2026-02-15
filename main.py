bc943c1587a8f8b88d48d72d441842bd224efb7b
Austin Rein <austin@reinresearch.org>
2026-02-15 15:32:18 -0500

    updated main.py and added vt_enrich.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class IP_Report(BaseModel):
+    ip: str
+    threat_score: float
+
 @app.get("/")
 async def root():
-    return {"message": "Hello world"}
+    return {"message": "Simple SOAR: A basic IP data ennrichment API"}
+
+@app.post("/report/", response_model=IP_Report)
+async def report_ip(report: IP_Report):
+    return report
+    
