ae1286d3a6b9cf19569ba90b3c34e7ef9c4925d1
Austin Rein <austinprein@pm.me>
2026-03-02 19:55:25 -0500

    Updated: ip_analysis.py
    Built out three more of the analysis functions and added error detection
    to prevent the final threat_score from being "tainted" by false
    negatives

--- src/ip_analysis.py
+++ src/ip_analysis.py
@@ -21,6 +21,11 @@ def calculate_vt_rating(vt_results):
         return None
 
     verdict = vt_results.get("verdict", {})
+    context = vt_results.get("context", {})
+
+    if "error" in verdict or "error" in context:
+            return None
+
     malicious_engines = verdict.get("malicious_score", 0)
     suspicious_engines = verdict.get("suspicious_score", 0)
     community_reputation = verdict.get("community_reputation", 0)
@@ -38,7 +43,7 @@ def calculate_vt_rating(vt_results):
         threat_rating += community_reputation_rating
 
     # Prevents a single API result from dicating the verdict too heavily
-    threat_rating = min(threat_rating, 100)
+    threat_rating = min(threat_rating, 100.0)
 
     return round(threat_rating, 2)
     
@@ -50,9 +55,15 @@ def calculate_aipdb_rating(aipdb_results):
         return None
     
     verdict = aipdb_results.get("verdict", {})
+    context = aipdb_results.get("context", {})
+
+    if "error" in verdict or "error" in context:
+            return None
+
     threat_rating = verdict.get("abuse_score", 0)
 
-    return round(threat_rating, 2)
+    # Casting as a float for consistency
+    return float(round(threat_rating, 2))
 
 def calculate_gn_rating(gn_results):
     threat_rating = 0.0
@@ -60,21 +71,87 @@ def calculate_gn_rating(gn_results):
     if not gn_results:
         return None
     
-    verdict = gn_results.get("verdict ", {})
+    verdict = gn_results.get("verdict", {})
+    context = gn_results.get("context", {})
 
-    if (verdict.get("rule_it_out", {}) == "true"):
-        return threat_rating
+    if "error" in verdict or "error" in context:
+        return None
 
-    if (verdict.get("mass_scanner", {}) == "true"):
-        threat_rating = 25.0
+    if (verdict.get("mass_scanner") is False and 
+        verdict.get("rule_it_out") is False and 
+        context.get("classification", "Unknown").lower() == "unknown"):
+        return None
+
+    classification = context.get("classification", "Unknown").lower()
+
+    if verdict.get("rule_it_out", {}) is True or classification == "benign":
+        return 0.0
+
+    if classification == "malicious":
+        threat_rating = 100.0
+
+    if verdict.get("mass_scanner", {}) is True and classification != "malicious":
+        threat_rating = 50.0
 
     return threat_rating
 
 def calculate_shodan_rating(shodan_results):
-    return None
+    threat_rating = 0.0
+
+    if not shodan_results:
+        return None
+
+    verdict = shodan_results.get("verdict", {})
+    context = shodan_results.get("context", {})
+
+    if "error" in verdict or "error" in context:
+            return None
+
+    vulnerabilities = verdict.get("vulnerabilites", [])
+    if vulnerabilities:
+        threat_rating += min(len(vulnerabilities) * 25.0, 50.0)
+
+    risky_ports = { 
+            21, 23, 69, 161, 162, 3389, 5900, 5985, 5986, 111, 135, 139, 445,
+            873, 2049, 1433, 1521, 3306, 5432, 5984, 6379, 9200, 11211, 27017
+        }
+
+    open_ports = verdict.get("open_ports", [])
+    open_risky_ports = set(open_ports).intersection(risky_ports)
+
+    if open_risky_ports:
+        threat_rating += (len(open_risky_ports) * 10.0)
+
+    if not open_ports and not vulnerabilities:
+        return None
+
+    threat_rating = min(threat_rating, 100.0)
+
+    return round(threat_rating, 2)
 
 def calculate_av_rating(av_results):
-    return None
+    threat_rating = 0.0
+
+    if not av_results:
+        return None
+    
+    verdict = av_results.get("verdict", {})
+    context = av_results.get("context", {})
+
+    if "error" in verdict or "error" in context:
+        return None
+
+    pulse_count = verdict.get("pulse_count", 0)
+    reputation_score = verdict.get("reputation_score", 0)
+
+    threat_rating += reputation_score
+
+    if pulse_count > 0:
+        threat_rating += min(pulse_count * 10.0, 50.0)
+
+    threat_rating = min(threat_rating, 100.0)
+
+    return round(threat_rating, 2)
 
 def analyze_ip_scan_results(results):
     total_rating = 0.0
@@ -90,6 +167,9 @@ def analyze_ip_scan_results(results):
 
     for calculation_function, result in zip(calculation_functions, results):
         rating = calculation_function(result)
+
+        #testing purposes
+        print(rating)
 
         if rating is not None:
             total_rating += rating
@@ -118,8 +198,8 @@ async def initiate_ip_scans(ip_address,env_variables):
     vt_results, aipdb_results, gn_results, shodan_results, av_results = results
     final_rating = analyze_ip_scan_results(results)
 
-    # Requires tuning
-    if final_rating > 80:
+    # 80 might be too high, reducing to 70 for now
+    if final_rating > 70:
         block = True
     else:
         block = False
